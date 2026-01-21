f43c87168dbacf268ef70ece96269a7940785169
somya-ctrl <agarwalsomya224@gmail.com>
2026-01-21 23:15:21 +0530

    socket-io setup done

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const http = require('http');
+const { Server } = require("socket.io");
 const authRoutes = require('./routes/user')
 const userRoutes = require('./routes/user')
 const {connectmongoDB} = require('./connection/user');
+
 
 
 const app = express();
@@ -13,6 +16,27 @@ app.use(express.urlencoded({extended:true}));
 
 app.use("/auth",authRoutes);
 app.use('/users',userRoutes);
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("joinChannel", (channelId) => {
+    socket.join(channelId);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
 const PORT = process.env.PORT || 3000;
 connectmongoDB(process.env.MONGO_URI);
-app.listen(PORT,()=> console.log(`server is running on port ${PORT}`));
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
