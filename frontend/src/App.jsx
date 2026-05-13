02e698d6540698edae36b1d0bdb1f5d757c8dc9a
somya-ctrl <agarwalsomya224@gmail.com>
2026-05-14 00:24:14 +0530

    signup page connected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LandingPage from "./pages/Landingpage";
 import SyncrovaLogin from "./pages/Login";
-
+import SignupPage from "./pages/signup";
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<SyncrovaLogin />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
