909d6daab599c310c42ae4d7f88a986a5a037381
somya-ctrl <agarwalsomya224@gmail.com>
2026-05-29 21:08:11 +0530

    helmet added

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
+const helmet = require("helmet");
 const express = require('express');
 
 const routes = require('./routes'); 
 
 const app = express();
 
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
